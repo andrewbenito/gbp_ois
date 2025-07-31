94801237d70a5e4a019708fabbe9d6a35055c43f
abenito <abenito.org@gmail.com>
2025-07-31 20:55:40 +0100

    drop here::here

--- gbp_ois/setup/01_OIS_curves.R
+++ gbp_ois/setup/01_OIS_curves.R
@@ -2,46 +2,54 @@
 # Download BoE data from url, Tidy and Plot
 
 # Packages ----
-lapply(c('here', 'tidyverse', 'readxl', 'lubridate', 'xts', 'showtext'),
-       require, character.only = TRUE)
-here::here()
+lapply(
+  c('tidyverse', 'readxl', 'lubridate', 'xts', 'showtext', 'dbnomics'),
+  require,
+  character.only = TRUE
+)
 
 # Settings: ggplot2 ----
 font_add_google("Roboto Condensed", "Roboto Condensed")
-theme_set(theme_bw(base_size = 15, base_family = "Roboto Condensed") +
-            theme(
-              text = element_text(family = "Roboto Condensed", size = 15), 
-              panel.background = element_rect(fill = "white"),
-              panel.grid.major = element_line(linewidth = .5),
-              panel.grid.minor = element_blank(),
-              axis.text = element_text(color = "dodgerblue")))
+theme_set(
+  theme_bw(base_size = 15, base_family = "Roboto Condensed") +
+    theme(
+      text = element_text(family = "Roboto Condensed", size = 15),
+      panel.background = element_rect(fill = "white"),
+      panel.grid.major = element_line(linewidth = .5),
+      panel.grid.minor = element_blank(),
+      axis.text = element_text(color = "dodgerblue")
+    )
+)
 showtext_auto()
 
 # Data: Download and Tidy  ----
 url <- "https://www.bankofengland.co.uk/-/media/boe/files/statistics/yield-curves/oisddata.zip"
-td <- tempdir() 
+td <- tempdir()
 tf <- tempfile(tmpdir = td, fileext = ".zip")
 download.file(url, tf)
-fname1 <- unzip(tf, list=TRUE)$Name[1]
-fname2 <- unzip(tf, list=TRUE)$Name[2]
-df1 <- read_xlsx(unzip(tf, files = fname1, exdir = td),
-                 sheet = "1. fwd curve") 
-df2 <- read_xlsx(unzip(tf, files = fname2, exdir = td),
-                 sheet = "1. fwds, short end") 
+fname1 <- unzip(tf, list = TRUE)$Name[1]
+fname2 <- unzip(tf, list = TRUE)$Name[2]
+df1 <- read_xlsx(unzip(tf, files = fname1, exdir = td), sheet = "1. fwd curve")
+df2 <- read_xlsx(
+  unzip(tf, files = fname2, exdir = td),
+  sheet = "1. fwds, short end"
+)
 
 # Latest OIS data ----
 url3 <- "https://www.bankofengland.co.uk/-/media/boe/files/statistics/yield-curves/latest-yield-curve-data.zip?la=en&hash=89B8A093FA97EF7DD79382044E15867840E45204"
-tf3  <- tempfile(tmpdir = td, fileext = ".zip")
+tf3 <- tempfile(tmpdir = td, fileext = ".zip")
 download.file(url3, tf3, mode = "wb")
 fname3 <- unzip(tf3, list = TRUE)$Name[4]
-df3    <- read_xlsx(unzip(tf3, files = fname3, exdir = td),
-                    sheet = "1. fwds, short end")
+df3 <- read_xlsx(
+  unzip(tf3, files = fname3, exdir = td),
+  sheet = "1. fwds, short end"
+)
 
 # Tidy Historic Forward Curve data ----
 cleanOIS <- function(df) {
   # Round the months row (excl date column)
   df[2, -1] <- round(df[2, -1], digits = 0)
-  
+
   df <- df %>%
     set_names(slice(., 2)) %>%
     tail(-5) %>%
@@ -50,7 +58,7 @@ cleanOIS <- function(df) {
     mutate(date = as.Date(date, origin = "1899-12-30")) %>% # Convert Excel dates
     drop_na() %>%
     column_to_rownames(var = "date")
-  
+
   return(df)
 }
 # Clean the 3 downloaded dataframes
@@ -60,41 +68,65 @@ for (dfn in c("df1", "df2", "df3")) {
 df <- bind_rows(df1, df2, df3) # Daily OIS data for inst fwds 1-60m
 
 # Convert Daily Data to Monthly; pivot----
-dfxts <- as.xts(df) 
-df_m  <- as.data.frame(apply.monthly(dfxts, mean)) 
+dfxts <- as.xts(df)
+df_m <- as.data.frame(apply.monthly(dfxts, mean))
 df_m$date = as_date(rownames(df_m))
 # Dates from maturities
-fwcv <- pivot_longer(df_m, !date, 
-                     names_to = "tau", values_to = "yield") %>%
-  mutate(month = month(date),
-         tau   = as.numeric(tau),
-         date2 = ymd(as.Date(date) %m+% months(tau)-1))
+fwcv <- pivot_longer(df_m, !date, names_to = "tau", values_to = "yield") %>%
+  mutate(
+    month = month(date),
+    tau = as.numeric(tau),
+    date2 = ymd(as.Date(date) %m+% months(tau) - 1)
+  )
 
 
 # Figure: Evolving Forwards----
-p1 <- ggplot(fwcv, aes(x=date2, y = yield, group = date)) +
-  geom_line(aes(colour = as.factor(date))) + 
-  theme(legend.position = "none") + 
-  labs(title = "GBP OIS Curves", 
-       subtitle = "monthly averages of end-of-day daily data",
-       x = "date", y = "rate %",
-       caption = "Source: Bank of England data")
-ggsave(here::here('plots', '1.GBP-OIS.png'), plot = p1, width = 9, height = 5, dpi = 90, units = "in", device='png')
+p1 <- ggplot(fwcv, aes(x = date2, y = yield, group = date)) +
+  geom_line(aes(colour = as.factor(date))) +
+  theme(legend.position = "none") +
+  labs(
+    title = "GBP OIS Curves",
+    subtitle = "monthly averages of end-of-day daily data",
+    x = "date",
+    y = "rate %",
+    caption = "Source: Bank of England data"
+  )
+# save plot
+ggsave(
+  filename = file.path("plots", "1.GBP-OIS.png"),
+  plot = p1,
+  width = 9,
+  height = 5,
+  dpi = 90,
+  units = "in",
+  device = 'png'
+)
 
 # Recent data
 comp.date <- last(fwcv$date) %m-% months(12)
 
-p2 <- ggplot(subset(fwcv, date>=as.Date(comp.date)), aes(x=date2, y = yield, group = date)) +
-  geom_line(aes(colour = as.factor(date))) + 
-  theme(legend.position = "none") + 
-  labs(title = "GBP OIS Curves: Past 12 months", 
-       subtitle = "monthly averages of end-of-day daily data",
-       x = "date", y = "rate %",
-       caption = "Source: Bank of England data")
-ggsave(here::here('plots', '2.GBP-OIS_12m.png'), plot = p2, width = 9, height = 5, dpi = 90, units = "in", device='png')
+p2 <- ggplot(
+  subset(fwcv, date >= as.Date(comp.date)),
+  aes(x = date2, y = yield, group = date)
+) +
+  geom_line(aes(colour = as.factor(date))) +
+  theme(legend.position = "none") +
+  labs(
+    title = "GBP OIS Curves: Past 12 months",
+    subtitle = "monthly averages of end-of-day daily data",
+    x = "date",
+    y = "rate %",
+    caption = "Source: Bank of England data"
+  )
+ggsave(
+  here::file.path('plots', '2.GBP-OIS_12m.png'),
+  plot = p2,
+  width = 9,
+  height = 5,
+  dpi = 90,
+  units = "in",
+  device = 'png'
+)
 
 # save data
-save(fwcv, file=here::here('data', 'data.Rda' ) )
-
-
-
+save(fwcv, file = file.path("gbp_ois", "data", "data.Rda"))
