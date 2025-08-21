9933654ae62d2ba8b3436d741ae11627b857f5a5
abenito <abenito.org@gmail.com>
2025-08-21 19:48:55 +0100

    added mpc voting file

---
+++ gbp_ois/setup/05_mpc_voting.R
@@ -0,0 +1,13 @@
+# 05_mpc_voting.R
+# Imports MPC voting data to ilustrate:
+# (i) extent of recent disagreement vs historical average
+# (ii) voting patterns of current MPC members
+
+# Import MPC voting Data ----
+url_voting <- "https://www.bankofengland.co.uk/-/media/boe/files/monetary-policy-summary-and-minutes/mpcvoting.xlsx"
+td <- tempdir()
+tf <- tempfile(tmpdir = td, fileext = ".xlsx")
+download.file(url_voting, tf, mode = "wb")
+voting <- read_xlsx(tf, sheet = "Bank Rate Decisions")
+
+# Clean MPC voting data ----
